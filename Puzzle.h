bfc86530be7f49281b0c8f53b35a4ec448e20e6d
Celeste <ccastil1@nd.edu>
2015-02-22 20:48:04 -0500

    new commit

--- Puzzle.h
+++ Puzzle.h
@@ -11,83 +11,89 @@
 #include <string>
 #include <cstdlib>
 #include <stdio.h>
+
 using namespace std;
 
-class Puzzle{
+typedef vector<int> oneVector;
+typedef vector<oneVector> twoDvector;
+typedef vector<vector<oneVector> > threeDvector;
+
+
+template<typename T>
+class Puzzle
+{
 public:
     Puzzle();   //declare default constructor
     Puzzle(string);   //declare non default constructor
     void print_puzzle();    //declare member function to print the puzzle
 private:
     int size; //declare size variable for board size
-    vector< vector<int> > SudokuBoardVec; //2D board vector
-    vector <vector <vector < vector<int> > > FillBoard; //3D vector to place numbers in board
+    twoDvector SudokuBoardVec; //2D board vector
+    threeDvector FillBoard; //3D vector to place numbers in board
 };
+#endif
 
-    template<typename T>
-    Puzzle<T>::Puzzle()
-    {
-        
-    }   //default constructor
+template<typename T>
+Puzzle<T>::Puzzle()
+{
     
+}   //default constructor
+
+
+template<typename T>
+Puzzle<T>::Puzzle(string file){
+    size = 9;
+    cout << "Enter a file name: ";
+    cin >> file;
     
-    template<typename T>
-    Puzzle<T>::Puzzle(file)
-    {
-        size = 9;
-        cout << "Enter a file name: ";
-        cin >> file;
-        
-        ifstream filename;
-        filename.open(file.c_str())
-        
-        int buffer;
-        for(int i=0; i<size; i++)
-        {
-            for(int j=0; j<size; j++)
-            {
-                if(!(filename >> buffer))
-                {
-                    cout << "Error! Invalid File!"<< endl;
-                    exit(1);
-                }
-                
-                if (buffer == 0 || buffer > 0 && buffer <= 9)
-                {
-                    SudokuBoardVec[i].push_back(buffer);
-                }
-            }
-        }
-        
-        for (int i=0; i < size; i++)
-        {
-            FillBoard.push_back(vector< vector<int>>());
-            for(int j=0; j<size; j++)
-            {
-                FillBoard[i].push_back(vector<int>());
-                for(int k=1; k < size; k++)
-                {
-                    FillBoard[i][j].push_back(k);
-                }
-                
-            }
-        }
-    }
+    ifstream filename;
+    filename.open(file.c_str());
     
-    template<typename T>
-    void Puzzle<T>::print_puzzle()
+    int buffer;
+    for(int i=0; i<size; i++)
     {
-        for(int i=0; i < size; i++)
+        for(int j=0; j<size; j++)
         {
-            for(int j=0; j < size; j++)
+            if(!(filename >> buffer))
             {
-                cout << SudokuBoardVec[i][j] << " ";
+                cout << "Error! Invalid File!"<< endl;
+                exit(1);
             }
             
-            cout << endl;
+            if (buffer == 0 || (buffer > 0 && buffer <= 9))
+            {
+                SudokuBoardVec[i].push_back(buffer);
+            }
         }
     }
+    
+    for (int i=0; i < size; i++)
+    {
+        FillBoard.push_back(vector<oneVector>());
+        for(int j=0; j<size; j++)
+        {
+            FillBoard[i].push_back(oneVector());
+            for(int k=1; k < size; k++)
+            {
+                FillBoard[i][j].push_back(k);
+            }
+            
+        }
+    }
+}
 
-#endif
+template<typename T>
+void Puzzle<T>::print_puzzle()
+{
+    for(int i=0; i < size; i++)
+    {
+        for(int j=0; j < size; j++)
+        {
+            cout << SudokuBoardVec[i][j] << " ";
+        }
+        
+        cout << endl;
+    }
+}
 
 
